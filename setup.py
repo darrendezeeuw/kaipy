fc78c2b185c24ac22a072cbdab4f23ae7f239556
Eric Winter <eric.winter@jhuapl.edu>
2024-11-19 12:32:11 -0500

    Fixed permissions. Bumped version to 1.0.8.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaipy',
-    version='1.0.7',
+    version='1.0.8',
     description='Python software for CGS MAGE and other Kaiju models',
     author='Kaiju team',
     author_email='wiltbemj@ucar.edu',
