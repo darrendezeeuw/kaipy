2329de072c6cec1122388c4f820af680279e066d
wiltbemj <wiltbemj@ucar.edu>
2025-02-13 14:37:45 -0700

    Tests for transform along with fixes to allow arrays

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -26,3 +26,47 @@ def test_GSEtoGSM():
     assert np.isclose(x_gsm, 1.0, atol=1e-3)
     assert np.isclose(y_gsm, 0.540, atol=1e-3)
     assert np.isclose(z_gsm, 3.565, atol=1e-3)
+
+def test_GSEtoGSM():
+    x, y, z = 1, 2, 3
+    ut = datetime.datetime(2009, 1, 27, 0, 0, 0)
+    x_gsm, y_gsm, z_gsm = GSEtoGSM(x, y, z, ut)
+    assert np.isclose(x_gsm, 1.0, atol=1e-3)
+    assert np.isclose(y_gsm, 0.540, atol=1e-3)
+    assert np.isclose(z_gsm, 3.565, atol=1e-3)
+
+def test_GSEtoGSM_zero():
+    x, y, z = 0, 0, 0
+    ut = datetime.datetime(2009, 1, 27, 0, 0, 0)
+    x_gsm, y_gsm, z_gsm = GSEtoGSM(x, y, z, ut)
+    assert np.isclose(x_gsm, 0.0, atol=1e-3)
+    assert np.isclose(y_gsm, 0.0, atol=1e-3)
+    assert np.isclose(z_gsm, 0.0, atol=1e-3)
+
+def test_GSEtoGSM_negative():
+    x, y, z = -1, -2, -3
+    ut = datetime.datetime(2009, 1, 27, 0, 0, 0)
+    x_gsm, y_gsm, z_gsm = GSEtoGSM(x, y, z, ut)
+    assert np.isclose(x_gsm, -1.0, atol=1e-3)
+    assert np.isclose(y_gsm, -0.540, atol=1e-3)
+    assert np.isclose(z_gsm, -3.565, atol=1e-3)
+
+def test_GSEtoGSM_large():
+    x, y, z = 1e6, 2e6, 3e6
+    ut = datetime.datetime(2009, 1, 27, 0, 0, 0)
+    x_gsm, y_gsm, z_gsm = GSEtoGSM(x, y, z, ut)
+    assert np.isclose(x_gsm, 1e6, atol=1e-3)
+    assert np.isclose(y_gsm, 540626.9291896636, atol=1e-3)
+    assert np.isclose(z_gsm, 3564789.2677457044, atol=1e-3)
+
+def test_GSEtoGSM_array():
+    x = np.array([1, 2, 3])
+    y = np.array([4, 5, 6])
+    z = np.array([7, 8, 9])
+    ut = np.array([datetime.datetime(2009, 1, 27, 0, 0, 0),
+                   datetime.datetime(2009, 1, 27, 1, 0, 0),
+                   datetime.datetime(2009, 1, 27, 2, 0, 0)])
+    x_gsm, y_gsm, z_gsm = GSEtoGSM(x, y, z, ut)
+    assert np.allclose(x_gsm, [1, 2, 3], atol=1e-3)
+    assert np.allclose(y_gsm, [0.657, 1.285, 2.0822], atol=1e-3)
+    assert np.allclose(z_gsm, [8.035, 9.3461, 10.614], atol=1e-3)
