15100001d5be410260c85d238fa22db5a2aa95fd
wiltbemj <wiltbemj@ucar.edu>
2025-04-24 09:42:16 -0600

    Remove InSetPosition since it is deprecated

--- kaipy/gamera/rcmpp.py
+++ kaipy/gamera/rcmpp.py
@@ -1,7 +1,6 @@
 #Various routines for plotting RCM output data
 import numpy as np
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
 import matplotlib.pyplot as plt
 import h5py
 import kaipy.kaiViz as kv
