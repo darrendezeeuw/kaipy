35c475f4a9af6d2c92b8cd92a4fdde0a5ddd3707
Anthony <asciola93@gmail.com>
2025-05-06 15:49:06 -0600

    Pulling over -ut option for printresTimes

--- kaipy/scripts/postproc/printResTimes.py
+++ kaipy/scripts/postproc/printResTimes.py
@@ -7,6 +7,8 @@ import subprocess
 import glob
 import argparse
 from argparse import RawTextHelpFormatter
+import h5py as h5
+import kaipy.kaiTools as kt
 
 def sortFn(elem): #Used to sort final list in order of nRes
     	return int(elem['nRes'])
@@ -21,27 +23,17 @@ def getAttrKeyValue(lineList):
 
 #Return dictionary of attrs for a single restart file
 def getKVsFromFile(fName):
-	spOutput = subprocess.check_output(['h5dump','-a','nRes','-a','t','-a','DATETIME',fName])
-	output = spOutput.decode('utf-8').split('\n')
-
-	#Parse attributes (this could be better)
-	attrLocs = []
-	for i in range(len(output)):
-		if 'ATTRIBUTE' in output[i]:
-			attrLocs.append(i)
 	
-	attrs = {} 
-	for i in range(len(attrLocs)):
-		if i == len(attrLocs)-1:
-			k,v = getAttrKeyValue(output[attrLocs[i]:])
-		else:
-			k,v = getAttrKeyValue(output[attrLocs[i]:attrLocs[i+1]])
-		attrs[k] = v
+	f5 = h5.File(fName)
+	attrs = {}
+	attrs['nRes'] = f5.attrs['nRes']
+	attrs['time'] = f5.attrs['time']
+	attrs['UT'] = kt.MJD2UT(f5.attrs['MJD'])
 	return attrs
 
+
 if __name__=='__main__':
-	idStr_noMPI = ".gam.Res.*.h5"
-	idStrMPI = "_0*_0*_0*_0000_0000_0000.gam.Res.*.h5"
+	idStr_noMPI = ".volt.Res.*.h5"
 
 	ftag = "msphere"
 	timeFmt = "m"
@@ -51,33 +43,29 @@ if __name__=='__main__':
 	parser = argparse.ArgumentParser(description=MainS, formatter_class=RawTextHelpFormatter)
 	parser.add_argument('-id',type=str,metavar="runid",default=ftag,help="RunID of data (default: %(default)s)")
 	parser.add_argument('-f',type=str,metavar="timeFmt",default=timeFmt,help="Time format [s,m,h] (default: %(default)s)")
-	parser.add_argument('-dt',action='store_true',default=False,help="Print the datetime instead of simulation time (default: %(default)s)")
+	parser.add_argument('-ut',action='store_true',default=False,help="Print UT instead of simulation time (default: %(default)s)")
 	args = parser.parse_args()
 	ftag = args.id
 	timeFormat = args.f
-	doDatetime = args.dt
+	doUT = args.ut
 
 	if timeFormat not in ['s','m','h']:
 		print('Unrecognized value "%s" for time format. Using "%s"'%(timeFormat, timeFmt))
 		timeFormat = timeFmt
 	if timeFormat == 's':
 		timeFormat = 'sec'
-		timeMult = 60
+		timeMult = 1
 	elif timeFormat == 'h':
 		timeFormat = 'hr'
-		timeMult = 1./60
+		timeMult = 1./3600
 	else:
 		timeFormat = 'min'
-		timeMult = 1
+		timeMult = 1./60
 	
 	#Get list of files
 	globStr = ftag + idStr_noMPI
-	print("Looking for nonMPI restarts...",end='')
+	print("Looking for voltron restarts...",end='')
 	fileList = glob.glob(globStr)
-	if len(fileList) == 0:
-		globStr = ftag + idStrMPI
-		print("Not found\nLooking for MPI restarts...",end='')
-		fileList = glob.glob(globStr)
 	if len(fileList) == 0:
 		print("Not found\nCheck id (globStr = %s)"%(globStr))
 		quit()
@@ -95,10 +83,10 @@ if __name__=='__main__':
 	#Print list (time from Gam restarts only, for now)
 	attrList.sort(key=sortFn)
 	for entry in attrList:
-		if doDatetime:
-			formatString = " {}: {}".format(entry['fname'], entry['DATETIME'])
+		if doUT:
+			formatString = " {}: {}".format(entry['fname'], entry['UT'])
 		else:
-			formatString = " {}: {:4.2f} [{}]".format(entry['fname'], float(entry['t'])*timeMult, timeFormat)
+			formatString = " {}: {:4.2f} [{}]".format(entry['fname'], float(entry['time'])*timeMult, timeFormat)
 		print(formatString)
 
 
